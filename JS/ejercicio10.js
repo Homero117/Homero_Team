de0bef7ee2553b5d061a5e49e7e709844550b6d3
Homero de Jesús Portillo Calva <115849319+Homero117@users.noreply.github.com>
2023-12-06 11:14:34 -0600

    Actualizar ejercicio10.js
    se elimino comentarios

--- JS/ejercicio10.js
+++ JS/ejercicio10.js
@@ -59,15 +59,15 @@ const recordCollection = {
 
 function updateRecords(records, id, prop, value) {
   if (value === "") {
-    // Si el valor es una cadena vacía, borra la propiedad "prop" del album.
+   
     delete records[id][prop];
   } else if (prop !== "tracks") {
-    // Si prop no es "tracks" y value no es una cadena vacía, asigna value a la propiedad "prop".
+   
     records[id][prop] = value;
   } else {
-    // Si prop es "tracks" y value no es una cadena vacía.
-    records[id][prop] = records[id][prop] || []; // Si no existe la propiedad "tracks", crea un arreglo vacío.
-    records[id][prop].push(value); // Añade "value" al final del arreglo.
+   
+    records[id][prop] = records[id][prop] || []; 
+    records[id][prop].push(value); 
   }
 
   return records;
